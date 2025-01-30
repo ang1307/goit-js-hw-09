74fca819bcf919e7707194b567fe54a8bafb3721
ang1307 <shaidiuk.angelina@gmail.com>
2025-01-30 18:37:16 +0200

    marcup

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,53 +2,47 @@ const feedbackForm = document.querySelector('.feedback-form');
 
 let formData = {
     email: "",
-    message:""
+    message: ""
 };
 
-const fillFormField = event => {
+const fillFormField = () => {
     try {
         const formDataFromLS = JSON.parse(localStorage.getItem("feedback-form-state"));
         if (formDataFromLS === null) {
             return;
         }
         formData = formDataFromLS;
-        console.log(formData);
         for (const key in formDataFromLS) {
             feedbackForm.elements[key].value = formDataFromLS[key]; 
-     }  
+        }  
     }
     catch (err) {
         console.log(err);
     }
 };
+
 fillFormField();
 
 const onFormFieldChange = event => {
     const formFieldEl = event.target;
-
-    const fieldValue = formFieldEl.value;
-    const fieldName = formFieldEl.name;
-
-    formData[fieldName] = fieldValue;
-
+    formData[formFieldEl.name] = formFieldEl.value;
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
-}
+};
 
 const onFeedbackFormSubmit = event => {
     event.preventDefault();
-  if (!(formData.email && formData.email.trim()) || !(formData.message && formData.message.trim())) {
+
+    if (!(formData.email && formData.email.trim()) || !(formData.message && formData.message.trim())) {
         alert('Fill please all fields');
         return;
     }
 
-    console.log(formData);
+    console.log(formData); // Виведення об'єкта тільки при сабміті
 
-    formData = {};
-    const formEl = event.currentTarget;
-    
-    formEl.reset();
+    formData = { email: "", message: "" }; // Очистка об'єкта
+    feedbackForm.reset();
     localStorage.removeItem("feedback-form-state");
-}
+};
 
 feedbackForm.addEventListener("input", onFormFieldChange);
 feedbackForm.addEventListener("submit", onFeedbackFormSubmit);
